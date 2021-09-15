061bf5130d0630d192128bcfa1e330f85e6c4312
Daniel Kish <aox.soljah@gmail.com>
2021-09-16 03:57:39 +0700

    finished all tasks

---
+++ user-onboarding/src/validation/formSchema.js
@@ -0,0 +1,27 @@
+import * as yup from 'yup';
+
+const formSchema = yup.object().shape({ 
+    first_name: yup
+        .string()
+        .trim()
+        .required('First name is required'),
+    last_name: yup
+        .string()
+        .trim()
+        .required('Last name is required'),
+    email: yup
+        .string()
+        .email('Must be a valid email address')
+        .required('email is required'),
+    password: yup
+        .string()
+        .required('A password is required')
+        .min(8, 'Password must be at least 8 characters long'),
+    tos: yup
+        .boolean()
+        .required('You must agree with the Terms Of Service')
+        .defined('aklsjajklsf')
+
+})
+
+export default formSchema;
